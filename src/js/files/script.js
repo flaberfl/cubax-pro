d83ac960ccb61477a6b01893adc6d8ec5ba88db5
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-03-03 16:29:29 +0300

    asd

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -9,11 +9,29 @@ import {
 
 
 
-document.onscroll = function () {
-  var dark = document.querySelector("#dark").getBoundingClientRect().top;
-  document.body.classList.toggle("dark", dark <= window.innerHeight);
-  var light = document.querySelector("#light").getBoundingClientRect().top;
+// document.onscroll = function () {
+//   var dark = document.querySelector("#dark").getBoundingClientRect().top;
+//   document.body.classList.toggle("dark", dark <= window.innerHeight);
+//   var light = document.querySelector("#light").getBoundingClientRect().top;
 
-  document.body.classList.remove("dark");
-  document.body.classList.toggle("light", light <= window.innerHeight);
+//   document.body.classList.remove("dark");
+//   document.body.classList.toggle("light", light <= window.innerHeight);
+// }
+
+
+if (
+  "IntersectionObserver" in window &&
+  "IntersectionObserverEntry" in window &&
+  "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+  let observer = new IntersectionObserver(entries => {
+    if (entries[0].boundingClientRect.y < 0) {
+      document.body.classList.add("header-not-at-top");
+    } else {
+      document.body.classList.remove("header-not-at-top");
+    }
+  });
+  observer.observe(document.querySelector("#anchor-black-th"));
 }
+
+console.log (ob)
